5822956be07f9d786a2fba3cb965858b3ec33c0d
gastbob40 <gastbob40@gmail.com>
2023-10-27 21:01:53 +0200

    feat(mangas): add refresh

--- lib/views/screens/manga_view.dart
+++ lib/views/screens/manga_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
@@ -26,57 +25,71 @@ class _MangaEntriesScreenState extends State<MangaEntriesScreen> {
     futureMangaEntries = apiService.getMangaEntries();
   }
 
+  Widget buildFutureManga() {
+    return FutureBuilder<List<MangaEntry>>(
+        future: futureMangaEntries,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No manga entries found.'));
+          } else {
+            return SingleChildScrollView(
+              child: LayoutGrid(
+                  columnSizes: [1.fr, 1.fr],
+                  // Adjust as needed for your design
+                  rowSizes: List.generate(
+                      snapshot.data!.length ~/ 2, (index) => auto),
+                  // Automatic row sizes
+                  children: [
+                    for (var index = 0; index < snapshot.data!.length; index++)
+                      GridPlacement(
+                          columnStart: index % 2,
+                          rowStart: index ~/ 2,
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                  builder: (context) => MangaChapterView(
+                                    mangaEntry: snapshot.data![index],
+                                  ),
+                                ),
+                              );
+                            },
+                            child: MangaCard(
+                              mangaEntry: snapshot.data![index],
+                            ),
+                          ))
+                  ]),
+            );
+          }
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(
         middle: Text('Manga Entries'),
       ),
-      child: SafeArea(
-          child: FutureBuilder<List<MangaEntry>>(
-            future: futureMangaEntries,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Center(child: Text('No manga entries found.'));
-              } else {
-
-                return SingleChildScrollView(
-                  child: LayoutGrid(
-                      columnSizes: [1.fr, 1.fr],
-                      // Adjust as needed for your design
-                      rowSizes: List.generate(
-                          snapshot.data!.length ~/ 2, (index) => auto),
-                      // Automatic row sizes
-                      children: [
-                        for (var index = 0; index < snapshot.data!.length; index++)
-                          GridPlacement(
-                              columnStart: index % 2,
-                              rowStart: index ~/ 2,
-                              child: GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    CupertinoPageRoute(
-                                      builder: (context) => MangaChapterView(
-                                          mangaEntry:  snapshot.data![index],
-                                      ),
-                                    ),
-                                  );
-                                },
-                                child: MangaCard(
-                                  mangaEntry: snapshot.data![index],
-                                ),
-                              )
-                          )
-                      ]),
-                );
-              }
+      child: SafeArea(child: CustomScrollView(
+        slivers: [
+          CupertinoSliverRefreshControl(
+            onRefresh: () async {
+              setState(() {
+                futureMangaEntries = apiService.getMangaEntries();
+              });
             },
-          )),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: buildFutureManga(),
+          )
+        ],
+      )),
     );
   }
 }
